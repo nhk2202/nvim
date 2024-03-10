7f4a613858bdb4ef4fa1cb9cb18fa35fe43fac3b
Hải Khánh <haikhanh220204@gmail.com>
2024-03-10 08:41:49 +0700

    Setup mini.clue.

--- init.lua
+++ init.lua
@@ -15,7 +15,6 @@ vim.o.smartcase = true
 vim.o.completeopt = "menuone,preview,noselect"
 vim.o.termguicolors = true
 vim.o.scrolloff = 10
-vim.o.timeoutlen = 500
 vim.o.showmode = false
 vim.o.undofile = true
 vim.o.inccommand = "split"
@@ -121,6 +120,26 @@ require("lazy").setup({
             vim.keymap.set('i', "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true })
             vim.keymap.set('i', "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
 
+            local miniclue = require("mini.clue")
+            miniclue.setup({
+                triggers = {
+                    { mode = 'n', keys = "<Leader>" },
+                    { mode = 'v', keys = "<Leader>" }
+                },
+
+                clues = {
+                    { mode = 'n', keys = "<Leader>p", desc = "+Pick" },
+                    { mode = 'n', keys = "<Leader>l", desc = "+LSP" },
+                },
+
+                window = {
+                    delay = 500,
+
+                    scroll_down = "<A-j>",
+                    scroll_up = "<A-k>"
+                }
+            })
+
             require("mini.comment").setup({
                 options = {
                     ignore_blank_line = true
@@ -191,24 +210,29 @@ require("lazy").setup({
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(ev)
                     vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-                    local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', "<Leader>lD", vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', "<Leader>ld", vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', "<Leader>l.", vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', "<Leader>li", vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', "<Leader>ls", vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', "<Leader>l+", vim.lsp.buf.add_workspace_folder, opts)
-                    vim.keymap.set('n', "<Leader>l-", vim.lsp.buf.remove_workspace_folder, opts)
+                    local opts = function(desc)
+                        return {
+                            buffer = ev.buf,
+                            desc = desc
+                        }
+                    end
+                    vim.keymap.set('n', "<Leader>lD", vim.lsp.buf.declaration, opts("Go to declaration"))
+                    vim.keymap.set('n', "<Leader>ld", vim.lsp.buf.definition, opts("Go to definition"))
+                    vim.keymap.set('n', "<Leader>l.", vim.lsp.buf.hover, opts("Show hover information"))
+                    vim.keymap.set('n', "<Leader>li", vim.lsp.buf.implementation, opts("Go to implementation"))
+                    vim.keymap.set('n', "<Leader>ls", vim.lsp.buf.signature_help, opts("Signature help"))
+                    vim.keymap.set('n', "<Leader>l+", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+                    vim.keymap.set('n', "<Leader>l-", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
                     vim.keymap.set('n', "<Leader>ll", function()
                         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                    end, opts)
-                    vim.keymap.set('n', "<Leader>lt", vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', "<Leader>lr", vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, "<Leader>la", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', "<Leader>l\"", vim.lsp.buf.references, opts)
+                    end, opts("List workspace folders" ))
+                    vim.keymap.set('n', "<Leader>lt", vim.lsp.buf.type_definition, opts("Go to type definition"))
+                    vim.keymap.set('n', "<Leader>lr", vim.lsp.buf.rename, opts("Rename buffer" ))
+                    vim.keymap.set({ 'n', 'v' }, "<Leader>la", vim.lsp.buf.code_action, opts("Code action"))
+                    vim.keymap.set('n', "<Leader>lr", vim.lsp.buf.references, opts("List references"))
                     vim.keymap.set('n', "<Leader>lf", function()
                         vim.lsp.buf.format({ async = true })
-                    end, opts)
+                    end, opts("Format code"))
                 end
             })
 
