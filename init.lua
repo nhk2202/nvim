c81c10b79b548d781639a1a7555485dca93a3914
Hải Khánh <haikhanh220204@gmail.com>
2024-11-07 21:05:26 +0700

    Add auto command to sync git blame output with file contents.

--- init.lua
+++ init.lua
@@ -136,6 +136,16 @@ require("lazy").setup({
 					}
 				})
 				vim.keymap.set('n', "<Leader>gg", MiniGit.show_at_cursor, { desc = "Show git data" })
+				local align_blame = function(au_data)
+					if au_data.data.git_subcommand ~= "blame" then return end
+					local win_src = au_data.data.win_source
+					vim.wo.wrap = false
+					vim.fn.winrestview({ topline = vim.fn.line('w0', win_src) })
+					vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+					vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+				end
+				local au_opts = { pattern = "MiniGitCommandSplit", callback = align_blame }
+				vim.api.nvim_create_autocmd("User", au_opts)
 
 				local hipatterns = require("mini.hipatterns")
 				hipatterns.setup({
