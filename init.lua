856bf2cef02144533a5bfcd1708c57e8b7e6239e
Hải Khánh <haikhanh220204@gmail.com>
2024-12-05 00:01:10 +0700

    Remove zenbones theme.

--- init.lua
+++ init.lua
@@ -254,13 +254,6 @@ require("lazy").setup({
             end
         },
 
-        {
-            "zenbones-theme/zenbones.nvim",
-            dependencies = "rktjmp/lush.nvim",
-            lazy = false,
-            priority = 1000,
-            config = function()
-                vim.cmd.colorscheme("zenwritten")
             end
         },
 
